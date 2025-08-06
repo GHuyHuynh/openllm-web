e60896a7c8c38849063b597b40af22e2d84b9179
GHuyHuynh <huy.huynh@dal.ca>
2025-08-05 22:43:33 -0300

    feat: rebuild homepage

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,35 +1,22 @@
-import { streamModelResponse } from '@/gen-ai/stream';
-import { useEffect, useState } from 'react';
-// import { ModelSelector } from '@/components/core/model-selector';
-// import { MultimodalInput } from '@/components/core/multimodal-input';
-import { Chat } from '../../components/core/chat';
+import { Chat } from '@/components/core/chat';
 import { DEFAULT_CHAT_MODEL } from '@/gen-ai/models';
+import { v4 as uuidv4 } from 'uuid';
+import { DataStreamHandler } from '@/components/core/data-stream-handler';
 
 export function HomePage() {
-  // TODO: Remove this 
-  const [_, setText] = useState('');
-
-  useEffect(() => {
-    const fetchText = async () => {
-      const result = await streamModelResponse('Write a short vegetarian pasta recipe.');
-
-      let accumulatedText = '';
-      for await (const textPart of result.textStream) {
-        accumulatedText += textPart;
-        setText(accumulatedText);
-      }
-    };
-
-    fetchText();
-  }, []);
+  const id = uuidv4();
 
   return (
-    <Chat
-      id={'1'}
-      initialMessages={[]}
-      initialChatModel={DEFAULT_CHAT_MODEL}
-      isReadonly={false}
-      autoResume={true}
-    />
-  )
+    <>
+      <Chat
+        key={id}
+        id={id}
+        initialMessages={[]}
+        initialChatModel={DEFAULT_CHAT_MODEL}
+        isReadonly={false}
+        autoResume={false}
+      />
+      <DataStreamHandler id={id} />
+    </>
+  );
 }
