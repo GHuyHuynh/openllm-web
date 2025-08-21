b34308f0f93ab1d3923e78505c5f7d110ffb147f
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2025-08-21 15:32:05 -0300

    fix: switching back to the correct model for vllm

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const VLLM_BASE_URL = 'http://129.173.22.43:30001';
-export const DEFAULT_VLLM_MODEL = 'ABJFSKJHJS';
+export const DEFAULT_VLLM_MODEL = 'meta-llama/Llama-3.2-1B-Instruct';
 export const DEFAULT_TITLE_MODEL = 'meta-llama/Llama-3.2-1B-Instruct';
 
 function calculateBaseUrl(): string {
