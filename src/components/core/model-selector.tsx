1f7432bd6cde3a92da444b72b29d01739a37acaf
GHuyHuynh <huy.huynh@dal.ca>
2025-08-27 21:04:33 -0300

    feat: add model name banner

--- src/components/core/model-selector.tsx
+++ src/components/core/model-selector.tsx
@@ -1,91 +1,19 @@
-import { startTransition, useMemo, useOptimistic, useState } from 'react';
-
+import meta from '@/assets/meta.svg';
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { cn } from '@/lib/utils';
-import { CheckCircleFillIcon, ChevronDownIcon } from '@/components/ui/icons';
-import { chatModels } from '@/ai-module/models';
 
 export function ModelSelector({
-  selectedModelId,
-  onModelChange,
   className,
 }: {
-  selectedModelId: string;
-  onModelChange?: (modelId: string) => void;
 } & React.ComponentProps<typeof Button>) {
-  const [open, setOpen] = useState(false);
-  const [optimisticModelId, setOptimisticModelId] =
-    useOptimistic(selectedModelId);
-
-  const selectedChatModel = useMemo(
-    () =>
-      chatModels.find((chatModel) => chatModel.id === optimisticModelId),
-    [optimisticModelId],
-  );
-
   return (
-    <DropdownMenu open={open} onOpenChange={setOpen}>
-      <DropdownMenuTrigger
-        asChild
-        className={cn(
-          'w-fit data-[state=open]:bg-accent data-[state=open]:text-accent-foreground',
-          className,
-        )}
-      >
-        <Button
-          data-testid="model-selector"
-          variant="outline"
-          className="md:px-2 md:h-[34px]"
-        >
-          {selectedChatModel?.name}
-          <ChevronDownIcon />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="start" className="min-w-[300px]">
-        {chatModels.map((chatModel) => {
-          const { id } = chatModel;
-
-          return (
-            <DropdownMenuItem
-              data-testid={`model-selector-item-${id}`}
-              key={id}
-              onSelect={() => {
-                setOpen(false);
-
-                startTransition(() => {
-                  setOptimisticModelId(id);
-                });
-
-                onModelChange?.(id);
-              }}
-              data-active={id === optimisticModelId}
-              asChild
-            >
-              <button
-                type="button"
-                className="gap-4 group/item flex flex-row justify-between items-center w-full"
-              >
-                <div className="flex flex-col gap-1 items-start">
-                  <div>{chatModel.name}</div>
-                  <div className="text-xs text-muted-foreground">
-                    {chatModel.description}
-                  </div>
-                </div>
-
-                <div className="text-foreground dark:text-foreground opacity-0 group-data-[active=true]/item:opacity-100">
-                  <CheckCircleFillIcon />
-                </div>
-              </button>
-            </DropdownMenuItem>
-          );
-        })}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      data-testid="model-selector"
+      variant="outline"
+      className={cn("md:px-2 md:h-[34px] cursor-default", className)}
+    >
+      <img src={meta} alt="Meta" className="w-4 h-4" />
+      Llama 3.2 1B
+    </Button>
   );
 }
